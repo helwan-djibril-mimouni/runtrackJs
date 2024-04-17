f16b64ce21f4ce9b8a2900009643ccda52ad4bc1
helwan-djibril-mimouni <163547268+helwan-djibril-mimouni@users.noreply.github.com>
2024-04-17 15:42:16 +0200

    Jour03 Job02 really done

--- Jour03/Job02/script.js
+++ Jour03/Job02/script.js
@@ -1,16 +1,11 @@
-$(document).ready(function(){
-    $("#button1").click(function(){
-        if (!show){
-            $("p").append("<li>Les logiciels et les cathédrales, c'est un peu la même chose - d'abord, on les construit, ensuite, on prie.</li>");
-            show = true
-        }
-    });
-    $("#button2").click(function(){
-        $("html").hide()
-    });
-});
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
     $( function() {
+      $("#sortable1").hide()
+      $("#verify").hide()
+
       $( "ul.droptrue" ).sortable({
         connectWith: "ul"
       });
@@ -18,6 +13,42 @@ $(document).ready(function(){
       $( "ul.dropfalse" ).sortable({
         connectWith: "ul",
         dropOnEmpty: false
+      });
+
+      $("#random").click(function(){
+        let shifts = 10
+        for (let index = 0; index < shifts; index++) {
+          let rand = getRandomInt(6)
+          let bool = getRandomInt(2)
+          let image1 = null
+          let image2 = null
+          if (rand == 0){
+            if (bool == 0){
+              $("#image1").before($("#image6"));
+            }
+            else{
+              $("#image1").after($("#image6"));
+            }
+          }
+          else {
+            $( "#sortable2" ).children('.to_check').each(function() {
+              if ($(this).attr('id') == "image" + (rand+1)){
+                image1 = this
+              }
+              if ($(this).attr('id') == "image" + (rand)){
+                image2 = this
+              }
+            });
+            if (bool == 0){
+              $(image1).before($(image2));
+            }
+            else{
+              $(image1).after($(image2));
+            }
+          }
+        }
+        $("#sortable1").show()
+        $("#verify").show()
       });
 
       $("#verify").click(function(){
@@ -28,18 +59,18 @@ $(document).ready(function(){
             }
             else{
               count = 0
+            }
+          });
+          if (count == 6){
+            $("p").text("Vous avez gagné")
+            $("p").css("color", "#00FF00")
+          }
+          else {
+            $("p").text("Vous avez perdu")
+            $("p").css("color", "#FF0000")
+            count = 0
           }
         });
-        if (count == 6){
-          $("p").text("Vous avez gagné")
-          $("p").css("color", "#00FF00")
-        }
-        else {
-          $("p").text("Vous avez perdu")
-          $("p").css("color", "#FF0000")
-          count = 0
-        }
-      });
    
-      $( "#sortable1, #sortable2, #sortable3" ).disableSelection();
-    } );
+        $( "#sortable1, #sortable2, #sortable3" ).disableSelection();
+      });
